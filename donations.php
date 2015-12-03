c09791cc041dd6bf83eddaa917f69f91a2a321ff
Binny V A <binnyva@gmail.com>
2015-12-03 20:39:34 +0530

    Disable adding and editing. Functional now

--- donations.php
+++ donations.php
@@ -2,6 +2,10 @@
 require('common.php');
 
 $crud = new Crud("external_donations");
+$crud->title = "External Donations";
+$crud->allow['add'] = false;
+$crud->allow['edit'] = false;
+
 $crud->setListingFields("donation_type", "donor_id", "fundraiser_id", "created_at", "donation_status");
 $crud->addListDataField("donor_id", "donours", "Donor", "", array('fields' => 'id,first_name'));
 $crud->addListDataField("fundraiser_id", "users", "Fundraiser", "", array('fields' => 'id,CONCAT(first_name, " ", last_name) AS name'));
