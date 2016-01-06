b1d79fc8ed5fb20f87cfa3ff9b1705e3b5b655e2
Binny V A <binnyva@gmail.com>
2016-01-06 19:22:44 +0530

    added divider lines

--- templates/coach_dashboard.php
+++ templates/coach_dashboard.php
@@ -1,26 +1,26 @@
 <h3>Donations raised by people coached by <?php echo $coach_name ?></h3>
 
 <table class="table table-striped">
-<tr><th>&nbsp;</th><th>Volunter</th><th colspan="2" class="amounts">100+</th><th colspan="2" class="amounts">12000+</th><th colspan="2" class="amounts">100000+</th></tr>
-<tr><th></th><th></th><th>Count</th><th>Total Amount</th><th>Count</th><th>Total Amount</th><th>Count</th><th>Total Amount</th></tr>
+<tr><th>&nbsp;</th><th class="line">Volunter</th><th colspan="2" class="amounts line">100+</th><th colspan="2" class="amounts line">12000+</th><th colspan="2" class="amounts">100000+</th></tr>
+<tr><th></th><th class="line"></th><th>Count</th><th class="line">Total Amount</th><th>Count</th><th class="line">Total Amount</th><th>Count</th><th>Total Amount</th></tr>
 <tr>
 	<td>All</td>
-	<td><?php echo count($all_volunteers) ?></td>
+	<td class="line"><?php echo count($all_volunteers) ?></td>
 	<td><?php echo $donations['total']['100'] ?></td>
-	<td><?php echo $donations['total']['100_amount'] ?></td>
+	<td class="line"><?php echo $donations['total']['100_amount'] ?></td>
 	<td><?php echo $donations['total']['12K'] ?></td>
-	<td><?php echo $donations['total']['12K_amount'] ?></td>
+	<td class="line"><?php echo $donations['total']['12K_amount'] ?></td>
 	<td><?php echo $donations['total']['1L'] ?></td>
 	<td><?php echo $donations['total']['1L_amount'] ?></td>
 </tr>
 <?php foreach ($all_volunteers as $volunteer_id => $volunteer_name) { ?>
 	<tr>
 		<td></td>
-		<td><a href="donation_dashboard.php?fundraiser_id=<?php echo $volunteer_id ?>"><?php echo $volunteer_name ?></a></td>
+		<td class="line"><a href="donation_dashboard.php?fundraiser_id=<?php echo $volunteer_id ?>"><?php echo $volunteer_name ?></a></td>
 		<td><?php echo $donations[$volunteer_id]['100'] ?></td>
-		<td><?php echo $donations[$volunteer_id]['100_amount'] ?></td>
+		<td class="line"><?php echo $donations[$volunteer_id]['100_amount'] ?></td>
 		<td><?php echo $donations[$volunteer_id]['12K'] ?></td>
-		<td><?php echo $donations[$volunteer_id]['12K_amount'] ?></td>
+		<td class="line"><?php echo $donations[$volunteer_id]['12K_amount'] ?></td>
 		<td><?php echo $donations[$volunteer_id]['1L'] ?></td>
 		<td><?php echo $donations[$volunteer_id]['1L_amount'] ?></td>
 	</tr>
--- templates/stratagy_dashboard.php
+++ templates/stratagy_dashboard.php
@@ -7,17 +7,17 @@ $html->buildInput("action", '&nbsp;', 'submit', 'Filter', array('class' => 'btn 
 </form><br /><br />
 
 <table class="table table-striped">
-<tr><th>Coach</th><th>Volunter Count</th><th colspan="3" class="amounts">100+</th><th colspan="3" class="amounts">12000+</th><th colspan="3" class="amounts">100000+</th></tr>
-<tr><th></th><th></th><th>Count</th><th>Percentage</th><th>Total Amount</th><th>Count</th><th>Percentage</th><th>Total Amount</th><th>Count</th><th>Percentage</th><th>Total Amount</th></tr>
+<tr><th>Coach</th><th class="line">Volunter Count</th><th colspan="3" class="amounts line">100+</th><th colspan="3" class="amounts line">12000+</th><th colspan="3" class="amounts">100000+</th></tr>
+<tr><th></th><th class="line"></th><th>Count</th><th>Percentage</th><th class="line">Total Amount</th><th>Count</th><th>Percentage</th><th class="line">Total Amount</th><th>Count</th><th>Percentage</th><th>Total Amount</th></tr>
 <tr>
 	<td>All</td>
-	<td><?php echo $total_volunteers ?></td>
+	<td class="line"><?php echo $total_volunteers ?></td>
 	<td><?php echo $donations['total']['100'] ?></td>
 	<td><?php echo $donations['total']['100_percent'] ?>%</td>
-	<td><?php echo $donations['total']['100_amount'] ?></td>
+	<td class="line"><?php echo $donations['total']['100_amount'] ?></td>
 	<td><?php echo $donations['total']['12K'] ?></td>
 	<td><?php echo $donations['total']['12K_percent'] ?>%</td>
-	<td><?php echo $donations['total']['12K_amount'] ?></td>
+	<td class="line"><?php echo $donations['total']['12K_amount'] ?></td>
 	<td><?php echo $donations['total']['1L'] ?></td>
 	<td><?php echo $donations['total']['1L_percent'] ?>%</td>
 	<td><?php echo $donations['total']['1L_amount'] ?></td>
@@ -25,13 +25,13 @@ $html->buildInput("action", '&nbsp;', 'submit', 'Filter', array('class' => 'btn 
 <?php foreach ($all_coaches as $coach_id => $coach_name) { ?>
 	<tr>
 		<td><a href="coach_dashboard.php?coach_id=<?php echo $coach_id ?>&amp;donation_status=<?php echo $donation_status ?>"><?php echo $coach_name ?></a></td>
-		<td><?php echo $couch_volunteers_count[$coach_id] ?></td>
+		<td class="line"><?php echo $couch_volunteers_count[$coach_id] ?></td>
 		<td><?php echo $donations[$coach_id]['100'] ?></td>
 		<td><?php echo $donations[$coach_id]['100_percent'] ?>%</td>
-		<td><?php echo $donations[$coach_id]['100_amount'] ?></td>
+		<td class="line"><?php echo $donations[$coach_id]['100_amount'] ?></td>
 		<td><?php echo $donations[$coach_id]['12K'] ?></td>
 		<td><?php echo $donations[$coach_id]['12K_percent'] ?>%</td>
-		<td><?php echo $donations[$coach_id]['12K_amount'] ?></td>
+		<td class="line"><?php echo $donations[$coach_id]['12K_amount'] ?></td>
 		<td><?php echo $donations[$coach_id]['1L'] ?></td>
 		<td><?php echo $donations[$coach_id]['1L_percent'] ?>%</td>
 		<td><?php echo $donations[$coach_id]['1L_amount'] ?></td>
