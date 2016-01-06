07b790c33c08f063fe3af94071b432af8e529675
Binny V A <binnyva@gmail.com>
2016-01-06 18:31:21 +0530

    Changed the title of the app

--- configuration.php
+++ configuration.php
@@ -1,7 +1,7 @@
 <?php
 //Configuration file for iFrame
 $config = array(
-	'site_title'	=> 'ExDon',
+	'site_title'	=> 'Strategy Dashboard',
 	'db_database'	=> ($_SERVER['HTTP_HOST'] == 'makeadiff.in') ? 'makeadiff_cfrapp' : 'Project_Donut',
 ) + $config_data;
 $config['site_home'] = $config_data['site_home'] . 'apps/exdon/';
