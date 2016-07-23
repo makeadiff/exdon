5e403d416012ce0b1e8239fea3894dd0c430368e
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-07-23 23:19:00 +0530

    Added ack email

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+
+
+require 'models/Email.php';
+
+
+
+$donor_name = "Sanjay";
+$amount = 10;
+$donor_email = "sanjay@makeadiff.in";
+$base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://'  . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/';
+$images[] = $base_url . 'assets/mad-letterhead-left.png';
+$images[] = $base_url . 'assets/mad-letterhead-logo.png';
+$images[] = $base_url . 'assets/mad-letterhead-right.png';
+
+
+$email = new Email();
+$email->html = '<html>
+						<head>
+						<title>Acknowledgement Email</title>
+						</head>
+						<body>
+						<table style="width: 960px;margin:0 auto;height: auto;border: 2px solid #f1f1f1;font-family:arial;font-size:20px;">
+						<tr><td style="vertical-align: top;"><img style="float:left;margin: 0px;" src="' . $base_url .  'assets/mad-letterhead-left.png' . '"/><img style="margin-left: -70px;" src="' . $base_url . 'assets/mad-letterhead-logo.png' . '"/><img style="float:right;margin:0px;" src="' . $base_url . 'assets/mad-letterhead-right.png' . '"/></td></tr>
+						<tr><td style="color:#cc2028;float:right;margin:10px 20px;"> ' . date("d/m/Y") . ' </td></tr>
+						<tr><td style="padding:10px 20px;"><strong>Dear ' . $donor_name . ',</strong></td></tr>
+						<tr><td style="padding:10px 20px;">Thanks a lot for your contribution of Rs.<strong style="color:#cc2028;">' . $amount . '/-</strong> towards Make A Difference.</td></tr>
+						<tr><td style="padding:10px 20px;">This is not a donation receipt. But only an acknowledgement. We will be sending you the e-receipt for the donation within the next 30 days once the amount reaches us.</td></tr>
+						<tr><td style="padding:10px 20px;">Please feel free to contact us on <a href="mailto:info@makeadiff.in">info@makeadiff.in</a> for any clarifications.</td></tr>
+						<tr><td style="padding:10px 20px;"><i>Little bit about Make A Difference: We are a youth run volunteer organization that  mobilizes young leaders to provide better outcomes to children living in shelter homes across India.</i></td></tr>
+						<tr><td style="padding:20px 20px;"><i>You can read more about us @ <a href="http://www.makeadiff.in"> www.makeadiff.in </a> | <a href="http://www.facebook.com/makeadiff"> www.facebook.com/makeadiff </a> | <a href="http://www.twitter.com/makeadiff">www.twitter.com/makeadiff</a></i></td></tr>
+						<tr><td style="color:#333231;font-size:16px;padding:0 20px;">First Floor, House no. 16C, MCHS colony, 1st B Main, 14th C Cross,</td></tr>
+						<tr><td style="color:#333231;font-size:16px;padding:0 20px;">HSR Layout, Sector 6, Bangalore - 560102.</td></tr>
+						<tr><td style="color:#333231;float:right;font-size:16px;margin:0 20px 20px;">http://www.makeadiff.in</td></tr>
+						</table>
+						</body>
+						</html>';
+$email->to = $donor_email;
+$email->from = "noreply <noreply@makeadiff.in>";
+$email->subject = "Donation Acknowledgment";
+$email->images = $images;
+
+$email->send();
