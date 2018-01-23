0883fd65f041b298e502a79fb1fe306e0e8d5652
Binny V A <binnyva@gmail.com>
2018-01-23 16:19:14 +0530

    NACH Approval System

--- nach/index.php
+++ nach/index.php
@@ -1,7 +1,13 @@
 <?php
 require '../common.php';
 
-if(i($QUERY, 'action')) {
+$all_cities = $sql->getById("SELECT id,name FROM cities ORDER BY name");
+$all_cities['0'] = 'All';
+$city_id = i($QUERY, 'city_id', 0);
+$action = i($QUERY, 'action');
+$html = new HTML;
+
+if($action == 'Approve') {
 	$donation_id = i($QUERY, 'donation_id');
 	$amount = i($QUERY, 'amount');
 
@@ -13,14 +19,24 @@ $status = [
 	'DEPOSIT COMPLETE'		=> 'Approved'
 ];
 
-$nach = new SqlPager("SELECT D.*, DON.first_name AS donor_name, DON.phone_no AS donor_phone, DON.email_id AS donor_email, U.first_name AS fundraiser 
+$where = ["1=1"];
+if($city_id) {
+	$where[] = "U.city_id=$city_id";
+}
+if($action == 'Filter') {
+	if(i($QUERY, 'donor_phone')) $where[] = "DON.phone_no LIKE '%".i($QUERY, 'donor_phone')."%'";
+	if(i($QUERY, 'donor_email')) $where[] = "DON.email_id LIKE '%".i($QUERY, 'donor_email')."%'";
+}
+
+$nach = new SqlPager("SELECT D.*, DON.first_name AS donor_name, DON.phone_no AS donor_phone, DON.email_id AS donor_email, U.first_name AS fundraiser, U.city_id
 	FROM external_donations D
 	INNER JOIN donours DON ON DON.id=donor_id
 	INNER JOIN users U ON U.id=D.fundraiser_id
-	WHERE donation_type = 'nach' AND D.created_at > '2017-08-01 00:00:00' -- AND donation_status='TO_BE_APPROVED_BY_POC'
+	WHERE donation_type = 'nach' AND D.created_at > '2017-08-01 00:00:00'
+	AND " . implode(" AND ", $where) . "
+	 -- AND donation_status='TO_BE_APPROVED_BY_POC'
 	ORDER BY created_at DESC", 50);
 
 $page = $nach->getPage();
-
 
 render();
--- templates/nach/index.php
+++ templates/nach/index.php
@@ -1,8 +1,19 @@
  
 <h1>NACH Approvals</h1>
 
+<form action="" method="post" class="form-area">
+<?php
+$html->buildInput("city_id", "City", 'select', $city_id, array('options' => $all_cities));
+$html->buildInput("donor_phone", "Donor Phone");
+$html->buildInput("donor_email", "Donor Email");
+$html->buildInput("action", "&nbsp;", 'submit', "Filter", ['class' => 'btn btn-success']);
+?>
+</form><br />
+
+<a href="http://makeadiff.in/apps/csvgo/index.php?name=approved_nach" class="with-icon done">CSV of Approved NACHs</a>
+
 <table class="table table-stripped">
-<tr><th>ID</th><th>Donor</th><th>Donor Phone</th><th>Donor Email</th><th>Fundraiser</th><th>Created At</th><th>Status</th><th>Amount</th><th>Action</th></tr>
+<tr><th>ID</th><th>Donor</th><th>Donor Phone</th><th>Donor Email</th><th>Fundraiser</th><th>City</th><th>Created At</th><th>Status</th><th>Amount</th><th>Action</th></tr>
 
 <?php foreach($page as $row) { ?>
 <tr>
@@ -12,10 +23,12 @@
 	<td><?php echo $row['donor_phone'] ?></td>
 	<td><?php echo $row['donor_email'] ?></td>
 	<td><?php echo $row['fundraiser'] ?></td>
+	<td><?php echo $all_cities[$row['city_id']] ?></td>
 	<td><?php echo date($config['date_format_php'], strtotime($row['created_at'])) ?></td>
 	<td><?php echo $status[$row['donation_status']]; ?></td>
 	<td><input type="text" value="<?php echo $row['amount'] ?>" name="amount" size="6" class="amount" />
-		<input type="hidden" value="<?php echo $row['id'] ?>" name="donation_id" /></td>
+		<input type="hidden" value="<?php echo $row['id'] ?>" name="donation_id" />
+		<input type="hidden" value="<?php echo $city_id ?>" name="city_id" /></td>
 	<td nowrap="1"><input type="submit" name="action" value="Approve" class="btn btn-primary" />
 	<?php if(i($QUERY, 'donation_id') == $row['id']) echo '<span class="icon done">Done</span>'; ?>
 	</td>
@@ -28,6 +41,8 @@
 $nach->link_template = '<a href="%%PAGE_LINK%%" class="%%CLASS%% with-icon">%%TEXT%%</a>';
 $nach->text['previous'] = $nach->text['next'] = $nach->text['first'] = $nach->text['last'] = '&nbsp;';
 
-print $nach->getLink("first") . ' | ' . $nach->getLink("previous");
-$nach->printPager();
-print $nach->getLink("next") . ' | ' . $nach->getLink("last");
+if($nach->total_pages > 1) {
+	print $nach->getLink("first") . ' | ' . $nach->getLink("previous");
+	$nach->printPager();
+	print $nach->getLink("next") . ' | ' . $nach->getLink("last");
+}
